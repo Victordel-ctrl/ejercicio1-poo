1a4f0b6a4e53b95fafd6896edfb6e7f0d719ae2e
Victor <victor@gmail.com>
2026-04-09 10:10:04 -0500

    Version 3

--- ejercicio.c
+++ ejercicio.c
@@ -4,7 +4,7 @@ int main() {
     float parcial1, parcial2, promedio;
 
     do {
-        printf("Ingrese la nota del primer parcial (0 - 20): ");
+        printf("Ingrese la nota del primer parcial: ");
         scanf("%f", &parcial1);
         if (parcial1 < 0 || parcial1 > 20) {
             printf("Error: la nota debe estar entre 0 y 20.\n");
@@ -12,7 +12,7 @@ int main() {
     } while (parcial1 < 0 || parcial1 > 20);
 
     do {
-        printf("Ingrese la nota del segundo parcial (0 - 20): ");
+        printf("Ingrese la nota del segundo parcial: ");
         scanf("%f", &parcial2);
         if (parcial2 < 0 || parcial2 > 20) {
             printf("Error: la nota debe estar entre 0 y 20.\n");
@@ -23,10 +23,12 @@ int main() {
 
     printf("Promedio: %.2f\n", promedio);
 
-    if (promedio >= 7) {
-        printf("Resultado: Aprobado\n");
+    if (promedio < 11) {
+        printf("Reprobado\n");
+    } else if (promedio >= 11 && promedio <= 13.99){
+        printf("Supletorios\n");
     } else {
-        printf("Resultado: Reprobado\n");
+        printf("Aprobado");
     }
 
     return 0;
